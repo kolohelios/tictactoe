cfc906126371c41a4b900913a1bb1254a540520d
Jon Edwards <jkedwards@me.com>
2015-04-09 13:29:31 -0700

    functioning game

--- client/index.js
+++ client/index.js
@@ -5,10 +5,14 @@ $(document).ready(init);
 function init(){
   $('#reset').click(reset);
   $('#start').click(start);
+  $('td').click(selecting);
 }
 
 function reset(){
   $('#chooser').show();
+  $('#players').hide();
+  $('td').css('background-color', 'white');
+  $('body').css('background-color', 'white');
 }
 
 function start(){
@@ -18,4 +22,83 @@ function start(){
   $('#p1').css('background-color', p1);
   $('#p2').css('background-color', p2);
   $('#chooser').hide();
+  var rnd = Math.floor(Math.random() * 2) + 1;
+  $('.player').removeClass('active');
+  $('#p' + rnd).addClass('active');
+}
+
+function selecting(){
+  var color = $('.active').css('background-color');
+  $(this).css('background-color', color);
+  $('.player').toggleClass('active');
+  gameCondition();
+}
+
+function gameCondition(){
+  var p1 = $('#p1').css('background-color');
+  var p2 = $('#p2').css('background-color');
+  var boardArray = [];
+  for(var i = 0; i < $('td').length; i++) {
+    var color = $('td').eq(i).css('background-color');
+    if(color === p1) {
+      boardArray.push('p1');
+    }
+    else if(color === p2) {
+      boardArray.push('p2');
+    }
+    else {
+      boardArray.push('');
+    }
+  }
+  var p1Array = [], p2Array = [];
+  boardArray.forEach(function(value, index) {
+    if(value === 'p1') {
+      p1Array.push(index);
+    }
+    else if(value === 'p2') {
+      p2Array.push(index);
+    }
+  });
+  var wins = [[0,1,2],[0,4,8],[0,3,6],[1,4,7],[2,5,8],[2,4,6],[3,4,5],[6,7,8]];
+  var p1matches = 0, p2matches = 0;
+  if(boardArray.indexOf('') === -1) {
+      gameEnd('dr');
+  }
+  wins.forEach(function(array) {
+    p1matches = 0;
+    p2matches = 0;
+    for(i = 0; i < 3; i++) {
+      if(p1Array.indexOf(array[i]) > -1) {
+        p1matches++;
+      }
+      else if(p2Array.indexOf(array[i]) > -1) {
+        p2matches++;
+      }
+    }
+    if(p1matches === 3) {
+      gameEnd('p1');
+    }
+    if(p2matches === 3) {
+      gameEnd('p2');
+    }
+  });
+}
+
+function gameEnd(winner) {
+  var p1 = $('#p1').css('background-color');
+  var p2 = $('#p2').css('background-color');
+  switch (winner) {
+    case 'dr':
+      $('body').css('background-color', 'blue');
+      $('body').append('<p>It was a draw.</p>');
+      break;
+    case 'p1':
+      $('body').append('<p>Player 1 won!</p>')
+      $('body').css('background-color', p1);
+      break;
+    case 'p2':
+      $('body').append('<p>Player 2 won!</p>')
+      $('body').css('background-color', p2);
+      break;
+  }
 }
