c886a3e14dc22059ee36451dc10ebea03185a711
Jon Edwards <jkedwards@me.com>
2015-04-09 10:26:37 -0700

    add ux for starting a new game

--- client/index.js
+++ client/index.js
@@ -1,1 +1,21 @@
 'use strict';
+
+$(document).ready(init);
+
+function init(){
+  $('#reset').click(reset);
+  $('#start').click(start);
+}
+
+function reset(){
+  $('#chooser').show();
+}
+
+function start(){
+  $('#players').show();
+  var p1 = $('#p1-choose').val();
+  var p2 = $('#p2-choose').val();
+  $('#p1').css('background-color', p1);
+  $('#p2').css('background-color', p2);
+  $('#chooser').hide();
+}
